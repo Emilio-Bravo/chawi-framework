8bbe576bd7eccb3214b980bfbf1a24224943dc4b
Emilio Bravo <78387018+Emilio-Bravo@users.noreply.github.com>
2021-06-17 17:13:41 -0500

    Update handlesRequestFiles.php

--- core/support/Files/handlesRequestFiles.php
+++ core/support/Files/handlesRequestFiles.php
@@ -4,14 +4,16 @@ namespace Core\Support\Files;
 
 trait HandlesRequestFiles
 {
-    public function hasFiles(): bool
+     public function hasFiles(): bool
     {
-        return !empty($_FILES);
+        foreach (array_keys($_FILES) as $key) {
+            return !empty($_FILES[$key]['name']);
+        }
     }
 
     public function hasFile($key)
     {
-        return isset($_FILES[$key]);
+        return isset($_FILES[$key]) && ($_FILES[$key]['name']) != null;
     }
 
     public function file($key)
